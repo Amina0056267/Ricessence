18b5a4dc595b1285985245545327848977928895
Amina Salah <145355192+AminaSalah098@users.noreply.github.com>
2024-11-20 16:40:20 +0000

    created new application (#8)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/components/header/navheader/navheader.component";
-import { FooterComponent } from "./shared/components/footer/footer/footer.component";
-import { HomepageComponent } from './homepage/homepage/homepage.component';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap'; // Import the carousel module
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomepageComponent, FooterComponent, NgbCarouselModule], // Add NgbCarouselModule here
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] 
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Ricessence';
